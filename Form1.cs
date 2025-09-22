9a89d5b6e34b526756a0e48c8bb9892eb6e0b500
NguyenQuocMinhHuy <nguyenquocminhhuy2004@gmail.com>
2025-09-22 10:26:44 +0700

    Revert

--- Form1.cs
+++ Form1.cs
@@ -17,42 +17,5 @@ namespace ProjectDemoGit_DuDiemQuaMon
         {
             InitializeComponent();
         }
-
-        private void label1_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void label2_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void label2_Click_1(object sender, EventArgs e)
-        {
-
-        }
-
-        private void Form1_Load(object sender, EventArgs e)
-        {
-
-        }
-
-        private void btnlogin_Click(object sender, EventArgs e)
-        {
-            string username = txtUserName.Text;
-            string password = txtpassword.Text;
-
-            if (username == "admin" && password == "admin")
-            {
-                SuccessForm successForm = new SuccessForm();
-                successForm.Show();
-                this.Hide();
-            }
-            else
-            {
-                MessageBox.Show("Tên đăng nhập hoặc mật khẩu không đúng!", "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
-            }
-        }
     }
 }
